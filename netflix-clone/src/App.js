84e3e7340528fe284b8003c6dc817aab9e4ce88d
waelalq <wael.abuomr@gmail.com>
2022-02-17 00:52:04 +0200

    add2

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -2,21 +2,21 @@ import * as React from "react";
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 // import { Switch } from 'react-router';
-import Navbar from './components/Navbar/Navbar'
+import Navbar2 from './components/Navbar/Navbar'
 import Home from "./components/Home/Home";
 // import Home from './components/Home/Home'
-// import Fav from './components/favPage/FavPage';
+import FavList from "./components/FavList/FavList";
 // import  Navbar  from './components/navbar/Navbar';
 
 function App() {
   return (
     <div>
-      <h1>kkk</h1>
-       {/* <Navbar/> */}
+    
+       <Navbar2/>
       <Routes>
      
           <Route path='/' exact  element={<Home/>} />
-          {/* <Route path='/favorite' exact element={<Fav/>} /> */}
+           <Route path='/favorite' exact element={<FavList/>} /> 
       
       </Routes>
     </div>
